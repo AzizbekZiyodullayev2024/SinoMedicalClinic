993dffe02fd446acbe105f43f188f19e628f2263
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-03-03 16:04:18 +0500

    Patch

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -7,7 +7,7 @@
                 </div>
                 <div class="h-100 d-inline-flex align-items-center py-3">
                     <small class="far fa-clock text-primary me-2"></small>
-                    <small>Dushanba - Shanba : 09.00 - 18:00</small>
+                    <small>Dushanba - Shanba : 08.00 - 22:00</small>
                 </div>
             </div>
             <div class="col-lg-5 px-5 text-end">
